916a06b82845a2bae0f5b74fbcc37c95fdfa2049
Alex Ng <ngjh1991@hotmail.com>
2017-10-14 20:04:35 +0800

    Add simple telegram bot command and message handler example

---
+++ kopi.py
@@ -0,0 +1,31 @@
+import json
+import logging
+
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler
+from telegram.ext import Filters
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+with open('config.json') as data_file:
+    data = json.load(data_file)
+
+updater = Updater(token=data['bot_token'])
+
+dispatcher = updater.dispatcher
+
+def start(bot, update):
+    bot.send_message(chat_id = update.message.chat_id, text = "I'm a bot, please talk to me!")
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
+
+def echo(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
