473fe9d12cbacac88e3c82e8d1fd1288a227a5fa
Alex Ng <ngjh1991@hotmail.com>
2017-10-15 11:45:08 +0800

    Resolve formatting issues

--- kopi.py
+++ kopi.py
@@ -16,16 +16,20 @@ updater = Updater(token=data['bot_token'])
 
 dispatcher = updater.dispatcher
 
+
 def start(bot, update):
-    bot.send_message(chat_id = update.message.chat_id, text = "I'm a bot, please talk to me!")
+    bot.send_message(chat_id=update.message.chat_id,
+                     text="I'm a bot, please talk to me!")
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
-updater.start_polling()
 
 def echo(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
 echo_handler = MessageHandler(Filters.text, echo)
 dispatcher.add_handler(echo_handler)
+
+updater.start_polling()
--- real_kopi.py
+++ real_kopi.py
@@ -11,28 +11,31 @@ from telegram.ext import CallbackQueryHandler
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
+
 def keyboard():
     keyboard = [[InlineKeyboardButton('Kopi', callback_data='Kopi'),
-                    InlineKeyboardButton('Teh', callback_data='Teh')],
+                 InlineKeyboardButton('Teh', callback_data='Teh')],
                 [InlineKeyboardButton('Kopi-O', callback_data='Kopi-O'),
-                    InlineKeyboardButton('Teh-O', callback_data='Teh-O')]]
+                 InlineKeyboardButton('Teh-O', callback_data='Teh-O')]]
     return keyboard
+
 
 def keyboard_reply_markup():
     return InlineKeyboardMarkup(keyboard())
 
+
 def start(bot, update):
     update.message.reply_text('%s started the coffee run, please order:' % update.message.from_user.first_name,
-                              reply_markup = keyboard_reply_markup())
+                              reply_markup=keyboard_reply_markup())
 
 
 def button(bot, update):
     query = update.callback_query
 
-    bot.edit_message_text(text = "%s\n%s selected option: %s" % (query.message.text, query.from_user.first_name, query.data),
-                          chat_id = query.message.chat_id,
-                          message_id = query.message.message_id,
-                          reply_markup = keyboard_reply_markup())
+    bot.edit_message_text(text="%s\n%s selected option: %s" % (query.message.text, query.from_user.first_name, query.data),
+                          chat_id=query.message.chat_id,
+                          message_id=query.message.message_id,
+                          reply_markup=keyboard_reply_markup())
 
 
 def help(bot, update):
