81e0d2b32ebf2a150350a1b82a1d0e6ed7765c8e
Alex Ng + LI Shunan + shermane lee <shunan@Shunans-MacBook-Pro.local>
2018-11-28 14:15:47 +0800

    Handle subscribing when subscriber file does not exist

--- coffee_tea_or_me/handlers/subscription_handler.py
+++ coffee_tea_or_me/handlers/subscription_handler.py
@@ -1,14 +1,16 @@
 from coffee_tea_or_me.helpers.helper import Helper
+import os
 
 class SubscriptionHandler:
 
 
     # Add a user to coffee run subscribers for a group chat
     def add_to_subscribers(chat_id, user_id):
-        with open(Helper.file_path('subscribers/%s.txt' % chat_id), 'r') as data_file:
-            for line in data_file:
-                if str(user_id) in line.rstrip():
-                    return
+        if os.path.exists(Helper.file_path('subscribers/%s.txt' % chat_id)):
+            with open(Helper.file_path('subscribers/%s.txt' % chat_id), 'r') as data_file:
+                for line in data_file:
+                    if str(user_id) in line.rstrip():
+                        return
 
         with open(Helper.file_path('subscribers/%s.txt' % chat_id), 'a+') as data_file:
             data_file.write(str(user_id) + '\n')
