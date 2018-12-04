4fe88c83703d510d7267c80073e6cfacd5f6ad29
ngjhalex <ngjh1991@hotmail.com>
2018-12-04 21:00:13 +0800

    Handle errors when sending notifications so that we can proceed to notify other subscribers when one fails

--- coffee_tea_or_me/handlers/notification_handler.py
+++ coffee_tea_or_me/handlers/notification_handler.py
@@ -1,5 +1,12 @@
+import logging
+
+from telegram.error import TelegramError
+
 from coffee_tea_or_me.handlers.keyboard_handler import KeyboardHandler
 from coffee_tea_or_me.helpers.helper import Helper
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 class NotificationHandler:
 
@@ -13,6 +20,9 @@ class NotificationHandler:
                 #                  % update.message.from_user.first_name,
                 #                  reply_markup=KeyboardHandler().keyboard_reply_markup(message))
 
-                bot.send_message(chat_id=line.rstrip(),
-                                 text="Stanly's Coffee is now open, please place your order:",
-                                 reply_markup=KeyboardHandler().keyboard_reply_markup(message))
+                try:
+                    bot.send_message(chat_id=line.rstrip(),
+                                     text="Stanly's Coffee is now open, please place your order:",
+                                     reply_markup=KeyboardHandler().keyboard_reply_markup(message))
+                except TelegramError as error:
+                    logging.warning('Unable to notify subscriber "%s", error: "%s"' % (line.rstrip(), error))
